2e6c8f6b84f19a2fec73dc436d2997e44ed3df4f
Michelle Lawand <120683133+mikiLawand@users.noreply.github.com>
2023-01-09 13:27:31 -0300

    Add files via upload

---
+++ estruturaFor.java
@@ -0,0 +1,12 @@
+public class estruturaFor {
+
+public static void main(String[] args) {
+    for ( int estrelinhas = 1; estrelinhas <= 13; estrelinhas ++) {
+        System.out.println("Contando as estrelinhas " + estrelinhas);
+    }
+
+    System.out.println("Gamora caiu no sono :)");
+}
+
+}
+
